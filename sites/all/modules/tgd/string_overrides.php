355e887ff899c869db90162202139d121fb8aa48
Jose Reyero <freelance@reyero.net>
2014-10-12 13:38:05 +0200

    Small tweaks for replacing 'groups/spaces' by 'areas'

---
+++ sites/all/modules/tgd/string_overrides.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @file
+ * String overrides for TGD
+ */
+
+$conf['locale_custom_strings_en'][''] = array(
+  'Spaces'      => 'Areas',
+  'Space'       => 'Area',
+  'spaces'      => 'areas',
+  'space'       => 'area',
+  'Subscribed Spaces' => 'Subscribed Areas',
+  'Public Spaces' => 'Public Areas',
+);
