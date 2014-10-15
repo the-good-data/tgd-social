6e05bfe934f7851f3d63040cf87858851a698006
Jose Reyero <freelance@reyero.net>
2014-10-15 10:18:57 +0200

    Created 'Subscribe to area to post comments' pane, added some more strings.

--- sites/all/modules/tgd/string_overrides.php
+++ sites/all/modules/tgd/string_overrides.php
@@ -13,4 +13,7 @@ $conf['locale_custom_strings_en'][''] = array(
   'Public Spaces' => 'Public Areas',
   'Subscribe to group' => 'Subscribe to area',
   'Unsubscribe from group' => 'Unsubscribe from area',
+  'Are you sure you want to unsubscribe from the group %title?' => 'Are you sure you want to unsubscribe from the area %title?',
+  'Are you sure you want to join the group %title?' => 'Are you sure you want to join the area %title?',
+
 );
